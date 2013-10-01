52c2f248530f8f5d321a609db724dac0d31fb478
Pradyut Panda <ppanda@zynga.com>
2013-09-30 21:45:52 -0700

    Initial Commit

---
+++ tipcalculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  tipcalculator
+//
+//  Created by ppanda on 9/30/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "xyzAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([xyzAppDelegate class]));
+    }
+}
---
+++ tipcalculator/xyzAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  xyzAppDelegate.h
+//  tipcalculator
+//
+//  Created by ppanda on 9/30/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface xyzAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
